4616fa51003659a6850caf84fdeb1efe9cfe16fe
qkrxodud00 <qkrxodud00@gmaill.com>
2022-06-21 17:25:58 +0900

    *swagger 설정

---
+++ src/main/java/triple/review/Swagger2Config.java
@@ -0,0 +1,38 @@
+package triple.review;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("triple.review"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("트리플 리뷰 포인트 조회 서비스")
+                .description("회원의 리뷰에 따른 포인트 부여")
+                .version("1.0.0")
+                .termsOfServiceUrl("https://https://cozing.oopy.io/")
+                .license("LICENSE")
+                .licenseUrl("")
+                .build();
+    }
+
+    // 완료가 되었으면 오른쪽 URL 로 접속 => http://localhost:8080/swagger-ui.html
+}
