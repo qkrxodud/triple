f0f13ac05421108c48c0be1aad3dd5e3d796df12
qkrxodud00 <qkrxodud00@gmaill.com>
2022-06-21 15:20:48 +0900

    *init commit

---
+++ src/main/java/triple/review/cotnroller/HelloController.java
@@ -0,0 +1,14 @@
+package triple.review.cotnroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+
+}
