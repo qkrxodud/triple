81a5239b9ad6df5c522d0958fe15e1c834a79c7e
qkrxodud00 <qkrxodud00@gmaill.com>
2022-06-21 21:43:12 +0900

    *리뷰, 첨부파일  -  존재 유무 쿼리 작성

--- src/main/java/triple/review/repository/AttachmentRepository.java
+++ src/main/java/triple/review/repository/AttachmentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import triple.review.entitiy.Attachment;
 
 public interface AttachmentRepository extends JpaRepository<Attachment, Integer> {
+    boolean existsByReview_ReviewIdAndReview_userUUID(Long reviewId, String userUUID);
 }
--- src/main/java/triple/review/repository/ReviewRepository.java
+++ src/main/java/triple/review/repository/ReviewRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import triple.review.entitiy.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+
+    boolean existsByUserUUIDAndPlaceUUID(String userUUID,String placeUUID);
+    boolean existsByPlaceUUID(String placeUUID);
 }
