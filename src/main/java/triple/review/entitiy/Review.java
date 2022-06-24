2051797a7da2cfe947aca67745dee8fe99527f98
qkrxodud00 <qkrxodud00@gmaill.com>
2022-06-24 17:27:21 +0900

    *Builder로 변경

--- src/main/java/triple/review/entitiy/Review.java
+++ src/main/java/triple/review/entitiy/Review.java
@@ -1,6 +1,8 @@
 package triple.review.entitiy;
 
+import com.mysema.commons.lang.Assert;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,6 +25,11 @@ public class Review {
     @Column(name = "place_uuid")
     private String placeUUID;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+
+    private ReviewStatus reviewStatus;
+
     private String content;
 
     //TODO BaseEntitiy로 할지 고민..
@@ -32,20 +39,41 @@ public class Review {
     private LocalDateTime modifyDate;
 
     public static Review createReview(String reviewUUID, String userUUID,
-                                      String placeUUID, String content) {
-        Review review = new Review();
-        review.changeReview(reviewUUID, userUUID, placeUUID, content);
+                                      String placeUUID, String content, ReviewStatus reviewStatus) {
+        Review review = Review.builder()
+                .reviewUUID(reviewUUID)
+                .userUUID(userUUID)
+                .placeUUID(placeUUID)
+                .content(content)
+                .reviewStatus(reviewStatus).build();
         return review;
     }
 
-    public void changeReview(String reviewUUID, String userUUID,
-                             String placeUUID, String content) {
+    @Builder
+    public Review(String reviewUUID, String userUUID,
+                  String placeUUID, String content, ReviewStatus reviewStatus) {
+
+        Assert.hasText(reviewUUID, "reviewUUID must not be empty");
+        Assert.hasText(userUUID, "userUUID must not be empty");
+        Assert.hasText(placeUUID, "placeUUID must not be empty");
+        Assert.hasText(content, "content must not be empty");
+        Assert.hasText(String.valueOf(reviewStatus), "reviewStatus must not be empty");
+
+
         this.reviewUUID = reviewUUID;
         this.userUUID = userUUID;
         this.placeUUID = placeUUID;
         this.content = content;
+        this.reviewStatus = reviewStatus;
         this.regDate = LocalDateTime.now();
         this.modifyDate = LocalDateTime.now();
     }
 
+    public void changeStatus(ReviewStatus reviewStatus) {
+        this.reviewStatus = reviewStatus;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
 }
