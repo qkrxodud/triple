9d9b3db130d8815f47dcf8c4a1d542d2215ceaa7
qkrxodud00 <qkrxodud00@gmaill.com>
2022-06-24 17:29:05 +0900

    *첨부파일 등록, 삭제 ,수정 추가

--- src/main/java/triple/review/repository/AttachmentRepository.java
+++ src/main/java/triple/review/repository/AttachmentRepository.java
@@ -3,6 +3,12 @@ package triple.review.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import triple.review.entitiy.Attachment;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AttachmentRepository extends JpaRepository<Attachment, Integer> {
     boolean existsByReview_ReviewIdAndReview_userUUID(Long reviewId, String userUUID);
+    boolean existsByReview_ReviewId(Long reviewId);
+    Optional<List<Attachment>> findByReview_ReviewId(Long reviewId);
+    void deleteByReview_ReviewId(Long reviewId);
 }
--- src/main/java/triple/review/service/AttachmentService.java
+++ src/main/java/triple/review/service/AttachmentService.java
@@ -4,11 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import triple.review.entitiy.Attachment;
+import triple.review.entitiy.Point;
 import triple.review.entitiy.PointStatus;
 import triple.review.entitiy.Review;
+import triple.review.exception.FileNotFoundException;
 import triple.review.repository.AttachmentRepository;
 import triple.review.repository.PointRepository;
 
+import java.util.List;
+
+import static triple.review.entitiy.Attachment.createAttachment;
 import static triple.review.entitiy.Point.createPoint;
 
 @Service
@@ -18,6 +23,15 @@ public class AttachmentService {
 
     private final AttachmentRepository attachmentRepository;
     private final PointRepository pointRepository;
+
+    // 파일 등록
+    @Transactional
+    public void saves(Review review, String [] fileUUIDs) {
+        for (String fileUUID : fileUUIDs) {
+            Attachment attachment = createAttachment(fileUUID, review);
+            save(attachment);
+        }
+    }
 
     // 파일 등록
     @Transactional
@@ -32,4 +46,51 @@ public class AttachmentService {
 
         return attachmentRepository.save(attachment);
     }
+
+    // 리뷰ID로 파일찾기
+    public List<Attachment> findByReviewId(Long reviewId) {
+        return attachmentRepository.findByReview_ReviewId(reviewId).orElseThrow(FileNotFoundException::new);
+    }
+
+    // 리뷰 파일 변경하기
+    @Transactional
+    public void changeAttachment(Review review, String [] fileUUIDs) {
+        // 기존에 첨부파일이 존재 했는지
+        boolean existsPreFile = attachmentRepository.existsByReview_ReviewId(review.getReviewId());
+
+        // 이전파일 O & 업데이트 파일 X
+        if (existsPreFile && !existsNowFile(fileUUIDs)) {
+            attachmentRepository.deleteByReview_ReviewId(review.getReviewId());
+            Point createPoint = createPoint(review, review.getUserUUID(), PointStatus.N, 1, "img_reg_delete");
+            pointRepository.save(createPoint);
+        // 이전파일 X & 업데이트 파일 O
+        } else if (!existsPreFile && existsNowFile(fileUUIDs)) {
+            for (String fileUUID : fileUUIDs) {
+                Attachment attachment = createAttachment(fileUUID, review);
+                save(attachment);
+            }
+        // 이전파일 O & 업데이트 파일 O
+        } else if (existsPreFile && existsNowFile(fileUUIDs)) {
+            attachmentRepository.deleteByReview_ReviewId(review.getReviewId());
+            for (String fileUUID : fileUUIDs) {
+                Attachment attachment = createAttachment(fileUUID, review);
+                attachmentRepository.save(attachment);
+            }
+
+        }
+    }
+
+    @Transactional
+    public void deleteAttachment(Review review) {
+        attachmentRepository.deleteByReview_ReviewId(review.getReviewId());
+    }
+
+    boolean existsNowFile(String [] fileUUIDs) {
+        boolean result = false;
+        if (fileUUIDs.length > 0) {
+            result = true;
+        }
+        return result;
+    }
+
 }
